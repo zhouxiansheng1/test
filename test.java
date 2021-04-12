58c924972b088b8b227eac33667ec5a5689e39fd
zhouchao <zhochaoxian>
2021-04-12 14:54:48 +0800

    测试

---
+++ test.java
@@ -0,0 +1,19 @@
+test.java
+public class GitTest{
+	private String git;
+	private String github;
+
+}
+class GetHubTest
+{
+	private String github1;
+
+}
+class Test
+{
+	psvm();
+}
+class Loder
+{
+	private String loder;
+}
